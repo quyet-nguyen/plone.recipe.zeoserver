ee9cb1946765aa6dee3f536a4b1c78f7f4eedad7
Quyet Nguyen <quyetnd112@gmail.com>
2018-04-13 15:45:38 +0700

    Fix grammar

--- src/plone/recipe/zeoserver/runzeo.bat
+++ src/plone/recipe/zeoserver/runzeo.bat
@@ -3,5 +3,4 @@
 @set ZODB3_HOME=%(zodb_home)s
 @set CONFIG_FILE=%(INSTANCE_HOME)s/etc/%(PACKAGE)s.conf
 @set PYTHONPATH=%(PYTHONPATH)s
-@set RUNZEO=-ui -m ZEO.runzeo 
-"%%PYTHON%%" "%%RUNZEO%%" -C "%%CONFIG_FILE%%" %%1 %%2 %%3 %%4 %%5 %%6 %%7 %%8 %%9
+"%%PYTHON%%" -ui -m ZEO.runzeo -C "%%CONFIG_FILE%%" %%1 %%2 %%3 %%4 %%5 %%6 %%7 %%8 %%9
